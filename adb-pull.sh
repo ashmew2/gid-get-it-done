69cfd420267da571437a289aa5eab01d23a8ecd2
Ashish Gupta <ashmew2@gmail.com>
2016-12-28 16:48:47 +0100

    implement adb-pull for file.

--- adb-pull.sh
+++ adb-pull.sh
@@ -2,19 +2,14 @@
 
 # Pull a file from a connected Android device.
 
-#ANDROID_FILE_PATH="/storage/sdcard/mytodo.txt"
-#ANDROID_FILE_PATH="/storage/emulated/todo.txt"
-ANDROID_FILE_PATH="/storage/sdcard1/efs/imei/bt.txt"
-
 #Hint: Use file manager on android, long press and details for file name.
 #If you dont have a details button, install a brain...err...a proper file manager.
-#ANDROID_FILE_PATH="/storage/emulated/todo.txt"
+ANDROID_FILE_PATH="/storage/sdcard/mytodo.txt"
 LOCAL_FILE_PATH="/tmp/mytodo.txt.android"
 
 adb start-server
 adb wait-for-device
-adb pull $ANDROID_FILE_PATH $LOCAL_FILE_PATH
+adb pull -p $ANDROID_FILE_PATH $LOCAL_FILE_PATH
 
 [[ $? -eq 0 ]] && echo "File pull Successful." && exit 0
 [[ $? -ne 0 ]] && echo "File pull Failed." && exit 1
-
